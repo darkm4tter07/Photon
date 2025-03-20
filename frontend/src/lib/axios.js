2de9b5f962802fba22432fd149c3853cc3e4959a
darkmatter <vikasanand.darkmatter@gmail.com>
2025-03-20 11:58:16 +0530

    chore: update API base URL to production and add CORS headers configuration

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE ==="development"? 'http://localhost:3000/api': '/api',
+    // baseURL: import.meta.env.MODE ==="development"? 'http://localhost:3000/api': 'https://photon-sepia.vercel.app/api',
+    baseURL: 'https://photon-sepia.vercel.app/api',
     withCredentials: true,
 });
