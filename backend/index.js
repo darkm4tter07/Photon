0d97726f08b07fa6f7576303c7f9d95e9dad0497
darkmatter <vikasanand.darkmatter@gmail.com>
2025-03-20 12:30:48 +0530

    fix: update CORS origin to use an array for better compatibility

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const port = 3000 || process.env.PORT;
 
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173"],
     credentials: true,
 }));
 app.use(express.json({ limit: '50mb' })); 
